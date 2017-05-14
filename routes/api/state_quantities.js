cd8639cc5d7b733dd3a31752e7df046a84e72f90
Dominick Triola <dominick@ignovadesign.com>
2017-05-13 23:20:44 -0700

    allow glassdoor query to be dynamic

--- routes/api/state_quantities.js
+++ routes/api/state_quantities.js
@@ -4,13 +4,15 @@ const rp = require('request-promise');
 const creds = require('../../config/config.js');
 
 router.get('/', (req, res, next) => {
+  const params = req.query;
+
   const options = {
     uri: `http://api.glassdoor.com/api/api.htm` +
       `?t.p=${creds.GD_ID}` +
       `&t.k=${creds.GD_KEY}` +
       `&userip=0.0.0.0&useragent=&format=json&v=1&action=jobs-stats` +
-      `&q=ruby` +
-      `&fromAge=3000` +
+      `&q=${params.q}` +
+      `&fromAge=${params.fromAge}` +
       `&returnStates=true` +
       `&admLevelRequested=1`,
     json: true
